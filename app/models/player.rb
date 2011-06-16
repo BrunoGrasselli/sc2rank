f7749cde0251b241c5973f0f84c18cefcd769ae1
Bruno Grasselli <bruno.grasselli@gmail.com>
2011-06-16 16:56:38 -0300

    parsing name and avatar

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,11 @@
 require 'open-uri'
 
 class Player < ActiveRecord::Base
-  def get_data
+  def parse_data
     doc = Nokogiri::HTML(open(profile_path))
+
     self.wins = doc.css('.campaign-casual h2').first.content
+    self.name = doc.css('#profile-header h2').first.content
+    self.avatar = doc.css('#portrait span').first["style"].scan(/url\(\'([^']+)\'\)/)[0][0]
   end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,22 +1,38 @@
 require 'spec_helper'
 
 describe Player do
-  describe '#get_data' do
+  describe '#parse_data' do
     before :each do
       body = %{
-        <div class='campaign-casual'>
-          <h2>150</h2>
+        <div id='profile-header'>
+          <h2>Grasselli</h2>
+          <div id="portrait">
+            <span style="background: url('/avatar.jpg?v20')">
+          </div>
         </div>
+        <div class='campaign-casual'><h2>150</h2></div>
       }
       stub_http_request(:get, 'http://userprofile.com').to_return body: body
 
       @player = Player.new profile_path: 'http://userprofile.com', initial_points: 149
     end
 
-    it "should update player wins" do
+    it "should update player's wins" do
       @player.wins = nil
-      @player.get_data
+      @player.parse_data
       @player.wins.should == 150
+    end
+
+    it "should update player's name" do
+      @player.name = nil
+      @player.parse_data
+      @player.name.should == "Grasselli"
+    end
+
+    it "should update player's avatar" do
+      @player.avatar = nil
+      @player.parse_data
+      @player.avatar.should == "/avatar.jpg?v20"
     end
   end
 end
