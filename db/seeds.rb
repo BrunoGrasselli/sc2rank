3d34c08921b275e08391303b9e04e8218a5d6cbb
Bruno Grasselli <bruno.grasselli@gmail.com>
2011-06-19 18:39:33 -0300

    fixing db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Player.create! :initial_points => 1, :profile_path => "http://us.battle.net/sc2/en/profile/323733/2/MoRReDiabo/"
-Player.create! :initial_points => 149, :profile_path => "http://us.battle.net/sc2/pt/profile/23540/2/Grasselli/"
-Player.create! :initial_points => 7, :profile_path => "http://us.battle.net/sc2/en/profile/320007/2/dynaum/"
-Player.create! :initial_points => 99, :profile_path => "http://us.battle.net/sc2/en/profile/2136/2/renttor/"
+Player.create! :initial_points => 1, :profile_path => "http://us.battle.net/sc2/en/profile/323733/2/MoRReDiabo/", :wins => 0
+Player.create! :initial_points => 149, :profile_path => "http://us.battle.net/sc2/pt/profile/23540/2/Grasselli/", :wins => 0
+Player.create! :initial_points => 7, :profile_path => "http://us.battle.net/sc2/en/profile/320007/2/dynaum/", :wins => 0
+Player.create! :initial_points => 99, :profile_path => "http://us.battle.net/sc2/en/profile/2136/2/renttor/", :wins => 0
