8b628a57f219de2b7a15a1fccca279b4a1a60758
Bruno Grasselli <bruno.grasselli@gmail.com>
2011-06-23 12:19:55 -0300

    joao added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,4 @@ Player.create! :initial_points => 149, :profile_path => "http://us.battle.net/sc
 Player.create! :initial_points => 7, :profile_path => "http://us.battle.net/sc2/en/profile/320007/2/dynaum/", :wins => 0
 Player.create! :initial_points => 99, :profile_path => "http://us.battle.net/sc2/en/profile/2136/2/renttor/", :wins => 0
 Player.create! :initial_points => 0, :profile_path => "http://us.battle.net/sc2/pt/profile/295629/2/Obiwan/", :wins => 0
+Player.create! :initial_points => 504, :profile_path => "http://us.battle.net/sc2/en/profile/217760/2/RANKAPREGA/", :wins => 0
