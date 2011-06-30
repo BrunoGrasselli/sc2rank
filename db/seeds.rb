fe90c9661b4564b40ab2baa4a6f55c58a8a2f67d
Elber Ribeiro <dynaumloko@gmail.com>
2011-06-30 19:00:02 -0300

    restart rank

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-Player.create! :initial_points => 1, :profile_path => "http://us.battle.net/sc2/en/profile/323733/2/MoRReDiabo/", :wins => 0
-Player.create! :initial_points => 149, :profile_path => "http://us.battle.net/sc2/pt/profile/23540/2/Grasselli/", :wins => 0
-Player.create! :initial_points => 7, :profile_path => "http://us.battle.net/sc2/en/profile/320007/2/dynaum/", :wins => 0
-Player.create! :initial_points => 99, :profile_path => "http://us.battle.net/sc2/en/profile/2136/2/renttor/", :wins => 0
-Player.create! :initial_points => 0, :profile_path => "http://us.battle.net/sc2/pt/profile/295629/2/Obiwan/", :wins => 0
-Player.create! :initial_points => 504, :profile_path => "http://us.battle.net/sc2/en/profile/217760/2/RANKAPREGA/", :wins => 0
+Player.create! :initial_points => 28, :profile_path => "http://us.battle.net/sc2/en/profile/323733/2/MoRReDiabo/", :wins => 0
+Player.create! :initial_points => 202, :profile_path => "http://us.battle.net/sc2/pt/profile/23540/2/Grasselli/", :wins => 0
+Player.create! :initial_points => 32, :profile_path => "http://us.battle.net/sc2/en/profile/320007/2/dynaum/", :wins => 0
+Player.create! :initial_points => 134, :profile_path => "http://us.battle.net/sc2/en/profile/2136/2/renttor/", :wins => 0
+Player.create! :initial_points => 14, :profile_path => "http://us.battle.net/sc2/pt/profile/295629/2/Obiwan/", :wins => 0
+Player.create! :initial_points => 505, :profile_path => "http://us.battle.net/sc2/en/profile/217760/2/RANKAPREGA/", :wins => 0
