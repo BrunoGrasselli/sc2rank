7a7b3fe8a7abf849c5eeffd1b0447f41d0fadf5c
Bruno Grasselli <bruno.grasselli@gmail.com>
2011-07-19 11:09:15 -0300

    Admin routes and headers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Sc2rank::Application.routes.draw do
   namespace :admin do
     resources :players
   end
+  match 'admin' => 'admin/players#index'
   match 'rank/update' => 'rank#update', :via => :put
   root :to => 'rank#index'
 end
